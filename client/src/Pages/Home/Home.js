6907fbc8f1a7023ba5af8c16d6ce0ac054953787
lautaroayosa <lautaroayosa@gmail.com>
2022-09-13 23:06:07 -0300

    Removed unnecessary useState

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import BlogsList from "../../components/Blogs/BlogsList/BlogsList";
 import Filter from '../../components/Filter/Filter'
