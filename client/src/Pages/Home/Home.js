eda3d82dffb3a962da81c76196f4cf6978db59dc
lautaroayosa <lautaroayosa@gmail.com>
2022-09-15 01:23:59 -0300

    Created the DashboardLayout component and modified the App file to work with the new layout

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -8,6 +8,10 @@ const Home = () => {
 
     return (
         <section className="homeContainer">
+            <div className="banner">
+                <h1>Welcome to "The Blog List"</h1>
+                <p>Have you ever found yourself searching for a new blog post to read? SAY NO MORE! The Blog List is the solution to that unbelievably common issue.</p>
+            </div>
             <Filter />
             <BlogsList/>
         </section>
