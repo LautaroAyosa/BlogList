d1d74ce5150edaafdf7689d8052fd8bdb4688bc5
lautaroayosa <lautaroayosa@gmail.com>
2022-09-18 00:41:54 -0300

    fixed eslint warnings

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Children, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from 'react-redux'
 import { initBlogs } from './redux/reducers/blogsReducer'
